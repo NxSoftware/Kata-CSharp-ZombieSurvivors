4a561c34f01363c5ce83c8732987d3d2e1ecd5ee
Steve Wilford <steve@stevewilford.co.uk>
2017-03-26 09:23:59 +0100

    Add survivor name

---
+++ ZombieSurvivors/Survivor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ZombieSurvivors
+{
+	public class Survivor
+	{
+		private readonly string _name;
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+		}
+
+		public Survivor(string name)
+		{
+			_name = name;
+		}
+	}
+}
---
+++ ZombieSurvivorsTests/SurvivorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ZombieSurvivors;
+using NUnit.Framework;
+
+namespace ZombieSurvivorsTests
+{
+	public class SurvivorTests
+	{
+		[Test]
+		public void CanCreateSurvivor()
+		{
+			var s = new Survivor("Steve");
+			Assert.AreEqual("Steve", s.Name);
+		}
+	}
+}
